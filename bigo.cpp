084165f941fb76d88051d97e5359b375c474ebc2
followthwhiterabbit <kaankaraalioglu@yahoo.com>
2023-02-06 23:49:41 +0100

    new push

--- bigo.cpp
+++ bigo.cpp
@@ -76,6 +76,87 @@ int pariSumSequence(int n)
 
 
 
+/*
+
+std::string reverses_string_with_spaces(std::string str)
+{
+    int n = str.size(); 
+
+    std::string result(n, '\0');
+
+
+
+    for(int i = 0; i < n; i++)
+        if(str[i] == ' ')
+            result[i] = ' '; 
+
+
+    // traverse input string from beginning
+    // and put characters in result from end 
+    int j = n - 1; 
+    for(int i = 0; i < str.length(); i++)
+        {
+            // ignore spaces in input string
+            if(str[i] != ' ')
+                {
+                    // ignore spaces in result
+                    while(result[j] == ' ')
+                        j--;
+
+                    result[j] = str[i];
+                    j--;  
+
+                }
+
+        }
+
+return result; 
+
+}
+*/ 
+
+
+
+bool isPrime(int n)
+{
+    for(int x = 2; x * x <= n; x++)
+    {
+        if(n % x == 0)
+            return false;
+    }
+
+return true; 
+
+}
+
+
+
+int factorial(int n)
+{
+    if(n < 0)
+        return -1;
+    else if (n == 0)
+        return 0; 
+    else
+        return n * factorial(n - 1);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+void print_vectors(std::vector<int>&, std::vector<int>&); 
+void print_another_vectors(std::vector<int>&, std::vector<int>&); 
+void reverse_array(std::vector<int>&);
+
+
 
 int main()
 {
@@ -83,10 +164,38 @@ int main()
 
 /*
 
+std::string my_name{"seyit kaan karaalioglu"};
+
+std::string reversed_string = reverses_string_with_spaces(my_name);
+
+std::cout << reversed_string << " "; 
+*/
+
+// std::cout << isPrime(5); 
+
+
+
+/*
+
+
+char name[5] = {"kaan"};
+
+name[4] = 'r';
+
+for(int i = 0; name[i] != '\0'; i++)
+    std::cout << name[i] << std::endl; 
+// int size = sizeof(name) / sizeof(name[0]);
+*/ 
+
+
+
+
+/*
+
 std::vector<int> myarr{1, 2, 3, 4, 5}; 
 
 int min = INT_MAX;
-int max = INT_MIN; 
+int max = INT_MIN;  
 
 std::cout << min << " " << max << std::endl; 
 
@@ -116,7 +225,134 @@ std::cout << max << std::endl;
 */ 
 
 
- 
+
+/*
+
+
+
+std::vector<int> vec{1, 2, 3, 4, 5};
+std::vector<int> vec2{6, 7, 8, 9, 10};
+
+for(auto &el : vec)
+    printf("%d ", el);
+
+printf("\n");
+
+for(auto &el : vec2)
+    printf("%d ", el); 
+
+printf("\n"); 
+
+std::cout << "O(A + B) runtime" << std::endl;  
+
+*/ 
+
+
+
+/*
+std::vector<int> vec{1, 2, 3, 4, 5};
+std::vector<int> vec2{6, 7, 8, 9, 10};
+
+for(int el : vec)
+    for(int el2 : vec2)
+        printf("%d-vec1 %d-vec2\n", el, el2); 
+
+*/ 
+
+/*
+Log N runtimes 
+search 9 within {1, 5, 8, 9, 11, 13, 15, 19, 21}
+    compare 9 to 11 -> smaller
+    search 9 within {1, 5, 8, 9}
+    compare 9 to 8 -> bigger
+        compare 9 to 9
+            return 
+
+we start off with an N-Element array to search. Then, after a single step, 
+we're down to N/2 elements. One more step and we're down to N/4 elements.
+We stop when we either find the value or we're down to just one element. 
+
+N = 16 
+N = 8  -> divide by 2
+N = 4 -> divide by 2 
+N = 2 -> divide by 2 
+N = 1 -> divide by 2
+
+2^4 = 16
+log 2 of 16 is gonna reveal 4 
+
+2^k = n  log 2 of N = k 
+
+RECURSIVE RUNTIMES
+
+int f(int n)
+{
+    if(n <= 1)
+        {
+            return 1; 
+        }
+    return f(n-1) + f(n-1); 
 
 }
+
+
+
+*/
+
+
+
+
+/*
+
+
+
+std::vector<int> vector1{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+std::vector<int> vector2{2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; 
+
+
+//print_vectors(vector1, vector2); // we needed to put the vector arguments as reference function parameters 
+//print_another_vectors(vector1, vector2); 
+reverse_array(vector1);
+
+for( auto &el : vector1)
+    std::cout << el << " "; 
+
+*/ 
+
+
+}
+
+void print_vectors(std::vector<int> &v1, std::vector<int> &v2)
+{
+
+    for(int i = 0 ; i < v1.size(); i++)
+       for(int j = i+1; j < v2.size(); j++)
+            std::cout << v1[i] << " " << v2[j] << std::endl; 
+
+}
+
+void print_another_vectors(std::vector<int> &v1, std::vector<int> &v2)
+{
+    for(int i = 0; i < v1.size(); i++)
+    {
+        for(int j = 0; j < v2.size(); j++)
+            {
+              if(v1[i] < v2[j]) // the if-statement within j's for loop is o(1) time since it's just a sequence of constnt time statements. 
+                std::cout << v1[i] << " " << v2[j] << std::endl;  
+            } }
+
+}
+
+void reverse_array(std::vector<int> &arr)
+{
+    for(int i = 0; i < arr.size() / 2; i++)
+    {
+        int end = arr.size() - i - 1;
+        int temp = arr[i]; 
+        arr[i] = arr[end]; 
+        arr[end] = temp; 
+    }
+
+}
+
 
