77940132cd1dc735cf93ad1c653023c0b8f76c05
followthwhiterabbit <kaankaraalioglu@yahoo.com>
2023-02-23 12:05:47 +0100

    one_way_solved

--- CHAPTER_1_ARRAYS_AND_STRINGS/one_way.cpp
+++ CHAPTER_1_ARRAYS_AND_STRINGS/one_way.cpp
@@ -18,6 +18,11 @@ pale, bake -> false // this have two edits
     let's first solve this question using maps 
  */
 
+
+/*
+
+// this is wrong 
+
 bool is_one_away(std::string &a, std::string &b)
 {
 int number_of_missing = 0; 
@@ -43,16 +48,57 @@ return true;
 
 }
 
+*/ 
+
+bool isOneAway(std::string s1, std::string s2)
+{
+    std::string a, b; 
+    a = s1.length() >= s2.length() ? s1 : s2; // a holds the greatest value 
+    b = s1.length() < s2.length() ? s1 : s2; // b holds the lower value  
+
+
+    int len1, len2; 
+    len1 = a.length(); // bigger value
+    len2 = b.length();  
+
+    if(abs(len1 -  len2) > 1) // if the changes are more than one, it's supposed to return false
+        return false;
+
+    bool flag = false; 
+
+
+    for(int i =0, j=0; i < len1 && j < len2;) // bares, pale 
+    {
+        if(a[i] != b[j])
+            {
+                if(flag)
+                    return false;
+                flag = true; 
+                if(len1 == len2)
+                    i++, j++; 
+                else
+                    i++; 
+            }
+        else
+            i++, j++; 
+    }
+
+return true;
+
+}
+
+
+
 
 int main()
 {
 
-std::string str_1 = "pale";
-std::string str_2 = "pale"; 
+std::string str_1 = "pbale";
+std::string str_2 = "bale"; 
 
 
-std::cout << is_one_away(str_1, str_2) << std::endl; 
-
+//std::cout << is_one_away(str_1, str_2) << std::endl; 
+std::cout << isOneAway(str_1, str_2) << std::endl; 
 }
 
 
