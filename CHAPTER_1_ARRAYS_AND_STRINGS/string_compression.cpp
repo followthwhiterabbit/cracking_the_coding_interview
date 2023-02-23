5b5a69f2e5a087b4448d3f2936ab35d23efee821
followthwhiterabbit <kaankaraalioglu@yahoo.com>
2023-02-23 17:33:10 +0100

    string_compensation solved

--- CHAPTER_1_ARRAYS_AND_STRINGS/string_compression.cpp
+++ CHAPTER_1_ARRAYS_AND_STRINGS/string_compression.cpp
@@ -12,32 +12,16 @@ else
     return output
 
 
-
-
-
-
-
  */
 #include <iostream>
-#include <map>
 
 
-int main()
+std::string string_compression(std::string &my_str)
 {
-    //std::multimap<char, int> multi_map; 
-    std::string my_str = "aabcccccaaa"; 
-    std::string res = "";
-    std::map<char, int> my_map;
-
-
-    for(auto &el : my_str)
-        my_str[el]++;
-
-
     int count_value = 1; 
+    std::string res = "";
 
-
-    for(int i = 0; i < my_str.size() - 1; i++)
+    for(int i = 0; i <= my_str.size() - 1; i++)
     {
 
         if(my_str[i] == my_str[i+1])
@@ -46,11 +30,32 @@ int main()
             {
                     res += my_str[i] + std::to_string(count_value); 
                     count_value = 1;
+
             }
      }
 
+    if(res.length() > my_str.length())
+        return my_str; 
+    else
+        return res; 
 
-    std::cout << res << std::endl; 
+
+}
+
+
+
+
+
+
+
+int main()
+{
+    //std::multimap<char, int> multi_map; 
+    //std::string my_str = "aabcccccaaa"; 
+     std::string my_str = "kkkaaabbbcccdddkkk"; 
+
+    std::cout << string_compression(my_str) << std::endl; 
+   
 
 
 }
