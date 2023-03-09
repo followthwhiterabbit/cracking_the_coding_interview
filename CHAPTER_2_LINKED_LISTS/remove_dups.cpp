f85d56f479adf9bdea70edd97b0d52056772c834
followthwhiterabbit <kaankaraalioglu@yahoo.com>
2023-03-09 22:57:33 +0100

    new_chapter_started

---
+++ CHAPTER_2_LINKED_LISTS/remove_dups.cpp
@@ -0,0 +1,49 @@
+/*
+Write code to remove duplicates from an unsorted linked list. 
+
+FOLLOW UP 
+How would you solve this problem if a temporary buffer is not allowed ? 
+
+*/
+
+#include <iostream>
+
+
+/*
+Since it's the first quesion, I would like to solve this problem without creating insertion method and 
+simply create different nodes and link them manually then send the head address to the remove duplicate function. 
+
+
+
+
+*/
+
+
+
+
+struct node
+{
+
+int data; 
+node* next; 
+
+}; 
+
+
+
+
+
+int main()
+{
+
+struct node* 
+
+
+
+
+
+
+}
+
+
+
