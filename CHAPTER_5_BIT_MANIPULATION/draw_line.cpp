8edc0ae497ed93455c94c6702764dcb709af88b8
followthwhiterabbit <kaankaraalioglu@yahoo.com>
2023-04-09 17:30:21 +0200

    draw line- i am not sure

--- CHAPTER_5_BIT_MANIPULATION/draw_line.cpp
+++ CHAPTER_5_BIT_MANIPULATION/draw_line.cpp
@@ -8,5 +8,39 @@ drawline(byte[] screen, int width, int x1, int x2, int y)
 
 
 
+
+
+
 */
+#include <iostream>
+#include "stddef.h"
+
+
+
+
+void drawline(byte screen[], int width, int x1, int x2, int y) {
+    int startByte = y * (width / 8) + x1 / 8;
+    int endByte = y * (width / 8) + x2 / 8;
+    byte startMask = (byte) (0xFF >> (x1 % 8));
+    byte endMask = (byte) ~(0xFF >> (x2 % 8 + 1));
+
+    if (startByte == endByte) {
+        screen[startByte] |= (startMask & endMask);
+    } else {
+        screen[startByte] |= startMask;
+        screen[endByte] |= endMask;
+        for (int i = startByte + 1; i < endByte; i++) {
+            screen[i] = (byte) 0xFF;
+        }
+    }
+}
+
+
+
+int main()
+{
+
+
+
+}
 
